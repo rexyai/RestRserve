3d90c0de427b52e5bdf9e8beeb28bae5a54ca2b6
Artem Klevtsov <a.a.klevtsov@gmail.com>
2018-01-10 21:18:55 +0700

    Add cases to test-server.R

--- tests/testthat/hello.R
+++ tests/testthat/hello.R
@@ -1,23 +1,26 @@
-calc_fib = function(n) {
-  if(n < 0L) stop("n should be >= 0")
-  if(n == 0L) return(0L)
-  if(n == 1L || n == 2L) return(1L)
-
-  x = rep(1L, n)
-  for(i in 3L:n)
-    x[[i]] = x[[i - 1]] + x[[i - 2]]
-
-  x[[n]]
-}
-
 fib = function(request) {
   try({n = as.integer( request$query[["n"]] )}, silent = TRUE)
 
   if((class(n) == "try-error") || length(request$query) != 1L)
     stop("request should look like 'n=5'")
 
-  RestRserve::create_response(body = as.character(calc_fib(n)), content_type = "text/plain",
-                              headers = character(0), status_code = 200L)
+  calc_fib = function(n) {
+    if(n < 0L) stop("n should be >= 0")
+    if(n == 0L) return(0L)
+    if(n == 1L || n == 2L) return(1L)
+
+    x = rep(1L, n)
+    for(i in 3L:n)
+      x[[i]] = x[[i - 1]] + x[[i - 2]]
+
+    x[[n]]
+  }
+
+  RestRserve::create_response(
+    body = as.character(calc_fib(n)),
+    content_type = "text/plain",
+    headers = character(0),
+    status_code = 200L)
 }
 
 # create application
--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -23,15 +23,24 @@ get_text <- function(url) {
   rawToChar(curl::curl_fetch_memory(url)$content)
 }
 
+test_200 <- "http://localhost:6666/fib?n=10"
+test_404 <- "http://localhost:6666/some-path"
+test_520 <- "http://localhost:6666/fib"
+
 test_that("Check status code", {
-    expect_equal(get_status_code("http://localhost:6666/bad-path"), 404L)
-    expect_equal(get_status_code("http://localhost:6666/fib?n=10"), 200L)
+    expect_equal(get_status_code(test_200), 200L)
+    expect_equal(get_status_code(test_404), 404L)
+    expect_equal(get_status_code(test_520), 520L)
 })
 
 test_that("Check headers", {
-    expect_equal(get_headers("http://localhost:6666/fib?n=10")$`content-type`, "text/plain")
+    expect_equal(get_headers(test_200)$`content-type`, "text/plain")
+    expect_equal(get_headers(test_404)$`content-type`, "text/plain")
+    expect_equal(get_headers(test_520)$`content-type`, "text/plain")
 })
 
 test_that("Check answer", {
-    expect_equal(get_text("http://localhost:6666/fib?n=10"), "55")
+    expect_equal(get_text(test_200), "55")
+    expect_equal(get_text(test_404), "Resource '/some-path' doesn't exist")
+    expect_match(get_text(test_520), "Error in R code. Traceback")
 })
