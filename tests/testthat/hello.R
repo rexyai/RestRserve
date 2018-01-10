9031bc537b42b7c07d1d2d74a743ab9b7a870780
Artem Klevtsov <a.a.klevtsov@gmail.com>
2018-01-10 20:49:00 +0700

    Add rserve test draft

---
+++ tests/testthat/hello.R
@@ -0,0 +1,28 @@
+calc_fib = function(n) {
+  if(n < 0L) stop("n should be >= 0")
+  if(n == 0L) return(0L)
+  if(n == 1L || n == 2L) return(1L)
+
+  x = rep(1L, n)
+  for(i in 3L:n)
+    x[[i]] = x[[i - 1]] + x[[i - 2]]
+
+  x[[n]]
+}
+
+fib = function(request) {
+  try({n = as.integer( request$query[["n"]] )}, silent = TRUE)
+
+  if((class(n) == "try-error") || length(request$query) != 1L)
+    stop("request should look like 'n=5'")
+
+  RestRserve::create_response(body = as.character(calc_fib(n)), content_type = "text/plain",
+                              headers = character(0), status_code = 200L)
+}
+
+# create application
+app = RestRserve::RestRserveApplication$new()
+# register endpoints and corresponding R handlers
+app$add_route(path = "/fib", method = "GET", FUN = fib)
+
+app$run(port = "6666")
---
+++ tests/testthat/test-server.R
@@ -0,0 +1,33 @@
+context("Rserve test")
+
+pid <- sys::exec_background(Sys.which("Rscript"), normalizePath("hello.R"))
+
+# Wait to Rserve up
+Sys.sleep(1)
+
+teardown(tools::pskill(pid))
+
+get_status_code <- function(url) {
+  curl::curl_fetch_memory(url)$status_code
+}
+
+get_headers <- function(url) {
+    curl::parse_headers_list(curl::curl_fetch_memory(url)$headers)
+}
+
+get_text <- function(url) {
+  rawToChar(curl::curl_fetch_memory(url)$content)
+}
+
+test_that("Check status code", {
+    expect_equal(get_status_code("http://localhost:6666/bad-path"), 404L)
+    expect_equal(get_status_code("http://localhost:6666/fib?n=10"), 200L)
+})
+
+test_that("Check headers", {
+    expect_equal(get_headers("http://localhost:6666/fib?n=10")$`content-type`, "text/plain")
+})
+
+test_that("Check answer", {
+    expect_equal(get_text("http://localhost:6666/fib?n=10"), "55")
+})
