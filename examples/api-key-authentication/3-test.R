63d5b17d43e72e7e14eb78e662e5886145efd5a5
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2018-03-05 17:31:36 +0400

    easier certificate generation

--- examples/api-key-authentication/3-test.R
+++ examples/api-key-authentication/3-test.R
@@ -4,6 +4,8 @@ n = 10
 
 message(sprintf("QUERING Fibonacci number for %d\n", n))
 
-n_fib = system2("curl", sprintf('-k  -H "X-API-KEY: d1d59815-515d-46b8-b890-b57439070e38" -X GET "https://localhost:%d/fib?n=%d"', HTTPS_PORT, n), stdout = TRUE)
+curl_args = sprintf('--insecure  -H "X-API-KEY: d1d59815-515d-46b8-b890-b57439070e38" -X GET "https://localhost:%d/fib?n=%d"', HTTPS_PORT, n)
+message("curl ", curl_args)
+n_fib = system2("curl", curl_args, stdout = TRUE)
 
 message(sprintf("\nANSWER: %s", n_fib))
--- examples/api-key-authentication/4-stop.R
+++ examples/api-key-authentication/4-stop.R
@@ -1,3 +1,5 @@
 #!/usr/bin/env Rscript
 DIR = "run"
 RestRserve::restrserve_stop(DIR)
+
+unlink(DIR, recursive = TRUE)
