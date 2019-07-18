006645711e6764fc82482b9d385665a895d1d3aa
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-18 22:24:25 +0700

    Decomposition parse_request for the better testing in future

--- R/RestRserveRequest.R
+++ R/RestRserveRequest.R
@@ -102,48 +102,22 @@ RestRserveRequest = R6::R6Class(
 #       \item{headers}{ = \code{c("a" = "1", "b" = "2")}, named character vector. key-value pairs from http-header.}
 #    }
 parse_request = function(path, query, body, headers) {
-  keys = names(query)
-  values = as.vector(query)
-
   # process query
-  query_dict = new.env(parent = emptyenv())
-  for(i in seq_along(keys)) {
-    key = keys[[i]]
-    value = values[[i]]
-    # see https://github.com/dselivanov/RestRserve/issues/24
-    # for explanation of this weird logic
-    if(key == "") {
-      key = value
-      value = NA_character_
-    }
-    query_dict[[key]] = value
-  }
+  query = parse_query(query)
 
   # process headers
-  if (is.raw(headers))
-    headers = rawToChar(headers)
+  headers = parse_headers(headers)
 
-  content_type = "application/octet-stream"
-  if (is.character(headers)) {
-    ## parse the headers into key/value pairs, collapsing multi-line values
-    header_lines = strsplit(gsub("[\r\n]+[ \t]+", " ", headers), "[\r\n]+")[[1]]
-    header_keys = tolower(gsub(":.*", "", header_lines))
-    header_values = gsub("^[^:]*:[[:space:]]*", "", header_lines)
-    i = grep("^[^:]+:", header_lines)
-    header_values = header_values[i]
-    header_keys = header_keys[i]
-    headers = combine_headers_by_key_env(header_keys, header_values)
-    # names(header_values) = header_keys
-    # Rserve adds "Request-Method: " key:
-    # https://github.com/s-u/Rserve/blob/05ff32d3c4512954a99162d392d0465d432d591e/src/http.c#L661
-    # according to the code above we assume that "request-method" is always exists
-    method = headers[["request-method"]]
-  } else {
-    headers = new.env(parent = emptyenv())
+  # Rserve adds "Request-Method: " key:
+  # https://github.com/s-u/Rserve/blob/05ff32d3c4512954a99162d392d0465d432d591e/src/http.c#L661
+  # according to the code above we assume that "request-method" is always exists
+  method = headers[["request-method"]]
+  if(is.null(method)) {
     method = "GET"
   }
 
   ## this is a bit convoluted - the HTTP already parses the body - disable it where you can
+  content_type = "application/octet-stream"
   if (!is.raw(body)) {
     if (length(body) > 0L) {
       body_keys = URLenc(names(body))
@@ -156,12 +130,59 @@ parse_request = function(path, query, body, headers) {
   } else {
     content_type = attr(body, "content-type")
   }
-  if(is.null(content_type)) content_type = "application/octet-stream"
 
-  RestRserveRequest$new(path = path,
-                        method = method,
-                        query = query_dict,
-                        headers = headers,
-                        body = body,
-                        content_type = content_type)
+  RestRserveRequest$new(
+    path = path,
+    method = method,
+    query = query,
+    headers = headers,
+    body = body,
+    content_type = content_type
+  )
+}
+
+# parse headers
+# combine http-headers by key
+# cookies processed in a special way - combined with "; " opposed to ", " for the rest of the keys
+parse_headers = function(headers) {
+  if (is.raw(headers)) {
+    headers = rawToChar(headers)
+  }
+  res = new.env(parent = emptyenv())
+  if (is.character(headers)) {
+    ## parse the headers into key/value pairs, collapsing multi-line values
+    lines = strsplit(gsub("[\r\n]+[ \t]+", " ", headers), "[\r\n]+")[[1]]
+    keys = tolower(gsub(":.*", "", lines))
+    values = gsub("^[^:]*:[[:space:]]*", "", lines)
+    idx = grep("^[^:]+:", lines)
+    keys = keys[idx]
+    values = values[idx]
+    for(i in seq_along(keys)) {
+      key = keys[[i]]
+      value = values[[i]]
+      # no such key yet
+      if(is.null(res[[key]])) {
+        res[[key]] = value
+      } else {
+        # key already exists and we need cobine values with existing values
+        if(key == "cookie") {
+          res[[key]] = paste(res[[key]], value, sep = "; ")
+        } else {
+          res[[key]] = paste(res[[key]], value, sep = ", ")
+        }
+      }
+    }
+  }
+  return(res)
+}
+
+# parse query
+parse_query = function(query) {
+  if (length(query) > 0L) {
+    res = as.list(query)
+    res = res[nzchar(names(res))]
+  } else {
+    res = list()
+  }
+  return(list2env(res))
 }
