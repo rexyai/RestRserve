8cc7893075493b8a4c4142570b98dfa6d539b7f3
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-21 23:29:47 +0700

    Add swagger server app tests

---
+++ tests/testthat/helper-app-swagger.R
@@ -0,0 +1,28 @@
+library(RestRserve)
+
+
+f1 = function(req, res) {
+  #' ----
+  #' description: f1
+  #'
+  #' parameters:
+  #'   - name: "n"
+  #'     required: true
+  #'----
+
+  #' just comment
+  NULL
+}
+
+openapi_flie = tempfile(fileext = ".yaml")
+swagger_ui_file = tempfile(fileext = ".html")
+app_swagger = RestRserveApplication$new()
+app_swagger$add_route(path = "/f1", method = "GET", FUN = f1)
+app_swagger$add_openapi(file_path = openapi_flie)
+app_swagger$add_swagger_ui(file_path = swagger_ui_file)
+
+
+app_swagger_port = 5566L
+
+
+# app_swagger$run(http.port = app_swagger_port, background = FALSE)
