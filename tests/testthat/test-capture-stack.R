bce2838c6f085d818fe66d98f0836ccc5ae4eb1e
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-30 22:09:14 +0700

    Add test for the get_traceback

--- tests/testthat/test-capture-stack.R
+++ tests/testthat/test-capture-stack.R
@@ -22,3 +22,16 @@ test_that("Nested call", {
   expect_equal(obj$calls[[1]], quote(f()))
   expect_equal(obj$calls[[2]], quote(stop("test")))
 })
+
+test_that("Get tracback", {
+  f = function() stop("test")
+  err = try_capture_stack(f())
+  obj = get_traceback(err)
+  expect_is(obj, "list")
+  expect_equal(names(obj), c("error", "call", "traceback"))
+  expect_length(obj$traceback, 2L)
+  expect_equal(obj$error, "test")
+  expect_equal(obj$call, "f()")
+  expect_equal(obj$traceback[[1]], "f()")
+  expect_equal(obj$traceback[[2]], "function() stop(\"test\")")
+})
