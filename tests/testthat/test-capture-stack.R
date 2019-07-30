924149a484552729b2753feb33685e35f64e456f
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-30 22:05:38 +0700

    Add tests for the try_capture_stack

---
+++ tests/testthat/test-capture-stack.R
@@ -0,0 +1,24 @@
+context("Test caputre stack")
+
+test_that("Normal call", {
+  expect_equal(try_capture_stack(1 + 1), 2)
+  expect_equal(try_capture_stack("test"), "test")
+})
+
+test_that("Single call", {
+  obj = try_capture_stack(stop("test"))
+  expect_is(obj, "simpleError")
+  expect_is(obj$calls, "list")
+  expect_length(obj$calls, 1L)
+  expect_equal(obj$calls[[1]], quote(stop("test")))
+})
+
+test_that("Nested call", {
+  f = function() stop("test")
+  obj = try_capture_stack(f())
+  expect_is(obj, "simpleError")
+  expect_is(obj$calls, "list")
+  expect_length(obj$calls, 2L)
+  expect_equal(obj$calls[[1]], quote(f()))
+  expect_equal(obj$calls[[2]], quote(stop("test")))
+})
