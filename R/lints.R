64c66a00fe7932c5ed2a7ee02284e98411930cc5
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-30 23:02:41 +0700

    Fix typo in `assignment_eq_linter`

--- R/lints.R
+++ R/lints.R
@@ -18,7 +18,7 @@ assignment_eq_linter = function(source_file) {
     lapply(ids_with_token(source_file, "LEFT_ASSIGN"), function(id) {
       parsed = with_id(source_file, id)
       lintr::Lint(filename = source_file$filename, line_number = parsed$line1,
-                  column_number = parsed$col1, type = "style", message = "Use =, not =, for assignment.",
+                  column_number = parsed$col1, type = "style", message = "Use =, not <-, for assignment.",
                   line = source_file$lines[as.character(parsed$line1)],
                   linter = "assignment_linter")
     })
