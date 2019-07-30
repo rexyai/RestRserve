640e60ca4ed2ced240e0ca3ac1397e76202158fd
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-30 21:48:42 +0700

    Remove dict_* functions

--- R/utils.R
+++ R/utils.R
@@ -5,49 +5,6 @@ URLenc = function(x) {
   x = as.character(x)
   vapply(x, utils::URLencode, character(1L), USE.NAMES = FALSE)
 }
-#------------------------------------------------
-# combine http-headers by key
-# cookies processed in a special way - combined with "; " opposed to ", " for the rest of the keys
-#------------------------------------------------
-combine_headers_by_key_env = function(header_keys, header_values) {
-  res = new.env(parent = emptyenv())
-
-  for (i in seq_along(header_keys)) {
-    key = header_keys[[i]]
-    value = header_values[[i]]
-    # no such key yet
-    if (is.null(res[[key]])) {
-      res[[key]] = value
-    } else {
-      # key already exists and we need cobine values with existing values
-      if (key == "cookie") {
-        res[[key]] = paste(res[[key]], value, sep = "; ")
-      } else {
-        res[[key]] = paste(res[[key]], value, sep = ", ")
-      }
-    }
-  }
-
-  res
-}
-#------------------------------------------------
-# environments as basic dictionaries
-#------------------------------------------------
-dict_create = function() {
-  new.env(parent = emptyenv())
-}
-dict_insert_not_empty = function(x, key, value) {
-  if (!is.environment(x))
-    stop("x should be environment")
-  if (!is.null(value) && length(value) > 0)
-    x[[key]] = value
-}
-dict_is_empty = function(x) {
-  if (!is.environment(x))
-    stop("x should be environment")
-  length(x) == 0L
-}
-#------------------------------------------------
 
 # borrowed from
 # https://github.com/r-lib/evaluate/blob/f0119259b3a1d335e399ac2235e91bb0e5b769b6/R/traceback.r#L29
@@ -89,4 +46,16 @@ guess_mime = function(file_path, content_type) {
   if (is.null(content_type))
     content_type = mime::guess_type(file_path)
   content_type
+}
+
+compact_list = function(x) {
+  x[lengths(x) > 0L]
+}
+
+is_string = function(x) {
+  is.character(x) && length(x) == 1L
+}
+
+is_path = function(path) {
+  is_string(path) && startsWith(path, "/")
 }
