6622cf0a89a62ba357c78ebc18cbe9c3f2951fed
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-08-02 11:42:32 +0700

    Simplify parse_cookies_str

--- src/parse_cookies.cpp
+++ src/parse_cookies.cpp
@@ -11,21 +11,12 @@ CharacterVector parse_cookies_str(std::string header) {
   if (starts_with(header, to_erase)) {
     header.erase(0, to_erase.length());
   }
+  std::string key, val;
   std::istringstream stream(header);
-  std::string buffer;
-  std::string::size_type index;
-  while (std::getline(stream, buffer, ';')) {
-    index = buffer.find('=', 0);
-    if(index != std::string::npos) {
-      std::string key = buffer.substr(0, index);
-      std::string val = buffer.substr(index + 1);
-      str_trim(key);
-      str_trim(val);
-      if (res.find(key) != res.end()) {
-        val = val + ", " + res[key];
-      }
-      res.insert(std::make_pair(key, val));
-    }
+  while (std::getline(std::getline(stream, key, '='), val, ';')) {
+    str_trim(key);
+    str_trim(val);
+    res.emplace(key, val);
   }
   return Rcpp::wrap(res);
 }
