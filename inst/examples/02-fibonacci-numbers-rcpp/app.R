95ffa3a4bfed2365db8bb787739e8a8cdc30bcd8
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-08-09 15:09:04 +0700

    Add 02-fibonacci-numbers-rcpp example

---
+++ inst/examples/02-fibonacci-numbers-rcpp/app.R
@@ -0,0 +1,52 @@
+#!/usr/bin/env Rscript
+
+## ---- load packages ----
+library(RestRserve)
+library(Rcpp)
+
+
+## ---- Functions ----
+
+# function to calc Fibonacci numbers
+sourceCpp(file.path("src", "fib.cpp"))
+
+
+## ---- create handler for the HTTP requests ----
+
+fib_handler = function(request, response) {
+  n = as.integer(request$query[["n"]])
+  if (length(n) == 0L || is.na(n)) {
+    err = HTTPErrorFactory$new(
+      content_type = "text/plain",
+      serializer = as.character
+    )
+    raise(err$bad_request())
+  }
+  response$body = calc_fib_cpp(n)
+  response$content_type = "text/plain"
+  response$serializer = as.character
+}
+
+
+## ---- create application -----
+
+app = RestRserveApplication$new(
+  content_type = "text/plain",
+  serializer = as.character
+)
+
+
+## ---- register endpoints and corresponding R handlers ----
+
+app$add_get(
+  path = "/fib",
+  FUN = fib_handler,
+  match = "exact"
+)
+
+
+## ---- start application ----
+
+app$run(
+  http_port = 8001
+)
