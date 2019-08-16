865865ca8c08a341574536cee97063b6ef31ee68
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-08-17 01:25:21 +0700

    Small refoctor multipart parser

--- src/multipart.cpp
+++ src/multipart.cpp
@@ -8,7 +8,6 @@
 #include <regex>
 #include "utils.h"
 
-
 struct MultipartFile {
   std::string filename;
   std::string content_type;
@@ -38,6 +37,8 @@ namespace Rcpp {
   }
 }
 
+using sv_size_t = std::string_view::size_type;
+using MultipartItem = std::pair<std::string,Rcpp::RObject>;
 using MultipartItems = std::unordered_multimap<std::string,Rcpp::RObject>;
 
 // [[Rcpp::export(rng=false)]]
@@ -47,6 +48,74 @@ std::string parse_multipart_boundary(const std::string& content_type) {
     throw(std::runtime_error("boundary string no found."));
   }
   return content_type.substr(pos + 9);
+}
+
+MultipartItem parse_multipart_block(std::string_view block, std::size_t offset) {
+  std::size_t block_n = block.size();
+
+  std::string name;
+  MultipartFile form_file;
+  MultipartValue form_value;
+  MultipartItem res;
+
+  bool found_cdisp = false;
+  bool found_ctype = false;
+  bool found_file = false;
+
+  // end of line string
+  static std::string eol = "\r\n";
+  // size of EOL string
+  static std::size_t eol_n = eol.size();
+
+  // regex to match Content-Disposition header
+  static std::regex re_cdisp(
+    "Content-Disposition: form-data; name=\"(.*?)\"(?:; filename=\"(.*?)\")?",
+    std::regex_constants::icase
+  );
+  // regex to match Content-Type header
+  static std::regex re_ctype(
+    "Content-Type: (.*?)",
+    std::regex_constants::icase
+  );
+
+  sv_size_t cur_pos = 0;
+  sv_size_t next_pos = std::string_view::npos;
+  while ((next_pos = block.find(eol, cur_pos)) != std::string_view::npos) {
+    auto line_n = next_pos - cur_pos;
+    std::string_view line = block.substr(cur_pos, line_n);
+    if (!line.empty()) {
+      std::cmatch m;
+      if (!found_cdisp && std::regex_match(line.begin(), line.end(), m, re_cdisp)) {
+        name = m[1];
+        form_file.filename = m[2];
+        found_cdisp = true;
+        found_file = !form_file.filename.empty();
+      }
+      if (found_file && !found_ctype && std::regex_match(line.begin(), line.end(), m, re_ctype)) {
+        form_file.content_type = m[1];
+        found_ctype = true;
+        // offset by current line
+        form_file.offset = next_pos;
+        // skip double empty line
+        form_file.offset += eol_n + eol_n;
+        // add one for the R
+        form_file.offset += 1;
+        form_file.length = block_n - form_file.offset - 1;
+        // correct to block position
+        form_file.offset += offset;
+      }
+      if (found_cdisp && !found_file) {
+        form_value.value = line;
+      }
+    }
+    cur_pos = next_pos + eol_n;
+  }
+  if (found_file && found_ctype) {
+    res = {name, Rcpp::wrap(form_file)};
+  } else {
+    res = {name, Rcpp::wrap(form_value)};
+  }
+  return res;
 }
 
 // [[Rcpp::export(rng=false)]]
@@ -67,20 +136,13 @@ Rcpp::List parse_multipart_body(Rcpp::RawVector body, const char* boundary) {
   static std::string eol = "\r\n";
   // size of EOL string
   static std::size_t eol_n = eol.size();
-  // regex to match Content-Disposition header
-  static std::regex re_cdisp(
-      "Content-Disposition: form-data; name=\"(.*?)\"(?:; filename=\"(.*?)\")?",
-      std::regex_constants::icase
-  );
-  // regex to match Content-Type header
-  static std::regex re_ctype(
-      "Content-Type: (.*?)",
-      std::regex_constants::icase
-  );
   // find boundary string
-  std::string_view::size_type block_start_pos = body_sv.find(boundary, 0);
+  sv_size_t block_start_pos = body_sv.find(boundary, 0);
+  if (block_start_pos == std::string_view::npos) {
+    return R_NilValue;
+  }
   // define end block
-  std::string_view::size_type block_end_pos = std::string_view::npos;
+  sv_size_t block_end_pos = std::string_view::npos;
   while (block_start_pos != std::string_view::npos) {
     // offset boundary string
     block_start_pos += boundary_n;
@@ -89,54 +151,9 @@ Rcpp::List parse_multipart_body(Rcpp::RawVector body, const char* boundary) {
     // find end of block
     block_end_pos = body_sv.find(boundary, block_start_pos);
     if (block_end_pos != std::string_view::npos) {
-      std::string name;
-      MultipartFile form_file;
-      MultipartValue form_value;
-
-      bool found_cdisp = false;
-      bool found_ctype = false;
-      bool found_file = false;
       auto block_size = block_end_pos - block_start_pos;
-      // offset eol before next boundary
       std::string_view block = body_sv.substr(block_start_pos, block_size);
-      auto line_start_pos = 0;
-      auto line_end_pos = block.find(eol);
-      while (line_end_pos != std::string_view::npos) {
-        if (found_cdisp && found_file && found_ctype) {
-          break;
-        }
-        auto line_size = line_end_pos - line_start_pos;
-        std::string_view line = block.substr(line_start_pos, line_size);
-        if (!line.empty()) {
-          std::cmatch m;
-          if (!found_cdisp && std::regex_match(line.begin(), line.end(), m, re_cdisp)) {
-            name = m[1];
-            form_file.filename = m[2];
-            found_cdisp = true;
-            found_file = !form_file.filename.empty();
-          }
-          if (found_file && !found_ctype && std::regex_match(line.begin(), line.end(), m, re_ctype)) {
-            form_file.content_type = m[1];
-            found_ctype = true;
-            form_file.offset = block_start_pos + line_end_pos;
-            // skip double empty line
-            form_file.offset += eol_n + eol_n;
-            // add one for the R
-            form_file.offset += 1;
-            form_file.length = block_end_pos - form_file.offset - 1;
-          }
-          if (found_cdisp && !found_file) {
-            form_value.value = line;
-          }
-        }
-        line_start_pos = line_end_pos + eol_n;
-        line_end_pos = block.find(eol, line_start_pos);
-      }
-      if (found_file && found_ctype) {
-        res.emplace(name, Rcpp::wrap(form_file));
-      } else {
-        res.emplace(name, Rcpp::wrap(form_value));
-      }
+      res.emplace(parse_multipart_block(block, block_start_pos));
     }
     // std::string_view block = body_sv.substr(start_pos + boundary_n + 2);
     block_start_pos = block_end_pos;
