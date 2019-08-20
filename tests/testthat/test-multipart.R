69d68093e8b093b72e5951b11bde60ec9f0d4ef8
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-08-20 11:59:01 +0700

    Remove offset from multipart test

--- tests/testthat/test-multipart.R
+++ tests/testthat/test-multipart.R
@@ -58,14 +58,12 @@ test_that("Test parse_multipart_body", {
 
   expect_equal(parsed$files[["desc_file.txt"]]$filename, basename(tmp_txt))
   expect_equal(parsed$files[["desc_file.txt"]]$content_type, "plain/text")
-  expect_equal(parsed$files[["desc_file.txt"]]$offset, 347)
   expect_equal(parsed$files[["desc_file.txt"]]$length, file.size(tmp_txt))
   expect_identical(get_multipart_file(body, parsed$files[["desc_file.txt"]]),
                    readBin(tmp_txt, raw(), file.size(tmp_txt)))
 
   expect_equal(parsed$files[["raw_file.bin"]]$filename, basename(tmp_rds))
   expect_equal(parsed$files[["raw_file.bin"]]$content_type, "application/octet-stream")
-  expect_equal(parsed$files[["raw_file.bin"]]$offset, 1652)
   expect_equal(parsed$files[["raw_file.bin"]]$length, file.size(tmp_rds))
   expect_identical(get_multipart_file(body, parsed$files[["raw_file.bin"]]),
                    readBin(tmp_rds, raw(), file.size(tmp_rds)))
