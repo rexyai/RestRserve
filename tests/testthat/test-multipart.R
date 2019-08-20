60f29f8e48aa06ea16478165b0a430562400f644
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-08-20 11:21:13 +0700

    Use real files sizes in test-multipart.R

--- tests/testthat/test-multipart.R
+++ tests/testthat/test-multipart.R
@@ -68,21 +68,21 @@ test_that("Test parse_multipart_body", {
   expect_equal(parsed$files[["desc_file.txt"]]$filename, basename(tmp_txt))
   expect_equal(parsed$files[["desc_file.txt"]]$content_type, "plain/text")
   expect_equal(parsed$files[["desc_file.txt"]]$offset, 347)
-  expect_equal(parsed$files[["desc_file.txt"]]$length, 1134)
+  expect_equal(parsed$files[["desc_file.txt"]]$length, file.size(tmp_txt))
   expect_identical(get_multipart_file(body, parsed$files[["desc_file.txt"]]),
                    readBin(tmp_txt, raw(), file.size(tmp_txt)))
 
   expect_equal(parsed$files[["raw_file.bin"]]$filename, basename(tmp_rds))
   expect_equal(parsed$files[["raw_file.bin"]]$content_type, "application/octet-stream")
   expect_equal(parsed$files[["raw_file.bin"]]$offset, 1653)
-  expect_equal(parsed$files[["raw_file.bin"]]$length, 111)
+  expect_equal(parsed$files[["raw_file.bin"]]$length, file.size(tmp_rds))
   expect_identical(get_multipart_file(body, parsed$files[["plog.png"]]),
                    readBin(tmp_png, raw(), file.size(tmp_png)))
 
   expect_equal(parsed$files[["plog.png"]]$filename, basename(tmp_png))
   expect_equal(parsed$files[["plog.png"]]$content_type, "image/png")
   expect_equal(parsed$files[["plog.png"]]$offset, 1917)
-  expect_equal(parsed$files[["plog.png"]]$length, 7714)
+  expect_equal(parsed$files[["plog.png"]]$length, file.size(tmp_png))
   expect_identical(get_multipart_file(body, parsed$files[["plog.png"]]),
                   readBin(tmp_png, raw(), file.size(tmp_png)))
 })
