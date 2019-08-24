4d40f76b565adc8661ddf6a8c810f8834ee0f903
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2019-08-24 18:41:09 +0400

    move deploy-start-stop to examples

---
+++ inst/examples/deploy-start-stop/app.R
@@ -0,0 +1,9 @@
+library(RestRserve)
+
+RestRserveApp = RestRserveApplication$new()
+
+RestRserveApp$add_get('/hello', function(request, response) {
+  response$body = 'Hello from RestRserve!'
+})
+
+
