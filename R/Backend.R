10aee2d69858bb7dec7dd8dbf11717520481312a
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2019-10-10 15:25:49 +0400

    remove methods from Backend class

--- R/Backend.R
+++ R/Backend.R
@@ -1,18 +1,12 @@
 Backend = R6::R6Class(
   "Backend",
-  private = list(
-    process_request = function(backend_request) {
-      request = self$set_request(backend_request)
-      response = app$process_request(request)
-      self$convert_response(response)
-    }
-  ),
   public = list(
     initialize = function() {},
     start = function(app, port, ...) {
       stop("not implemented")
     }
-  )
+  ),
+  private = list()
 )
 
 # TODO
@@ -20,10 +14,8 @@ BackendHttpuv = R6::R6Class(
   "BackendHttpuv",
   inherit = Backend,
   public = list(
-    start = function(app, http_port, ...) {
-      httpuv::startServer(host = "0.0.0.0", port = http_port, app = list(
-        call = private$process_request,
-      ))
-    }
-  ),
+    # start = function(app, http_port, ...) {
+    #   httpuv::startServer(host = "0.0.0.0", port = http_port, app = list(call = private$process_request))
+    # }
+  )
 )
