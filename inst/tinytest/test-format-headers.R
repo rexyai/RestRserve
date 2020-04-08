58172a81b2327e3bf2db33e7f4d12102ece057b9
Artem Klevtsov <a.a.klevtsov@gmail.com>
2020-04-08 22:24:13 +0700

    Fix tests

--- inst/tinytest/test-format-headers.R
+++ inst/tinytest/test-format-headers.R
@@ -1,17 +1,17 @@
 # Test format headers
 
 # import functions
-format_headers = RestRserve:::format_headers
+cpp_format_headers = RestRserve:::cpp_format_headers
 
 # Empty object
-expect_equal(format_headers(NULL), "")
-expect_equal(format_headers(list()), "")
-expect_equal(format_headers(list(a = "")), "")
-expect_equal(format_headers(list(a = character(0))), "")
-expect_error(format_headers(""))
-expect_error(format_headers(NA))
-expect_error(format_headers(list("")))
-expect_error(format_headers(list(a = NULL)))
+expect_equal(cpp_format_headers(NULL), "")
+expect_equal(cpp_format_headers(list()), "")
+expect_equal(cpp_format_headers(list(a = "")), "")
+expect_equal(cpp_format_headers(list(a = character(0))), "")
+expect_error(cpp_format_headers(""))
+expect_error(cpp_format_headers(NA))
+expect_error(cpp_format_headers(list("")))
+expect_error(cpp_format_headers(list(a = NULL)))
 
 # Test headers
 h = list(
@@ -19,7 +19,7 @@ h = list(
   "Content-Length" = 10L,
   "Custom" = c("value1", "value2")
 )
-r = format_headers(h)
+r = cpp_format_headers(h)
 v = paste(
   c("Content-Type: application/json",
     "Content-Length: 10",
