6bf406b4846e44920005c8b886977ed2b6049693
David Zimmermann-Kollenda <david.zimmermann-kollenda@genre.com>
2022-04-28 19:16:28 +0200

    add conditional header split

--- src/parse_headers.cpp
+++ src/parse_headers.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <unordered_map>
+#include <unordered_set>
 #include <Rcpp.h>
 #include "utils.h"
 
@@ -10,6 +11,41 @@ using Headers = std::unordered_map<std::string, std::vector<std::string>>;
 bool validate_header_name(const std::string& x) {
   const char* valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!#$%&'*+-.^_`|~";
   return x.find_first_not_of(valid) == std::string::npos;
+}
+// see also: https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+// also: https://stackoverflow.com/a/29550711/3048453
+const std::unordered_set<std::string> headers_to_split = {
+  "accept",
+  "accept-charset",
+  "access-control-request-headers",
+  "accept-encoding",
+  "accept-language",
+  "accept-patch",
+  "accept-ranges",
+  "allow",
+  "cache-control",
+  "connection",
+  "content-encoding",
+  "content-language",
+  "cookie",
+  "expect",
+  "forwarded",
+  "if-match",
+  "if-none-match",
+  "pragma",
+  "proxy-authenticate",
+  "te",
+  "trailer",
+  "transfer-encoding",
+  "upgrade",
+  "vary",
+  "via",
+  "warning",
+  "www-authenticate",
+  "x-forwarded-for"
+};
+bool split_header(const std::string& key) {
+  return headers_to_split.count(key) != 0;
 }
 
 // [[Rcpp::export(rng=false)]]
@@ -35,7 +71,14 @@ Rcpp::List cpp_parse_headers(const char* headers) {
       }
       char sep = key == "cookie" ? ';' : ',';
       std::vector<std::string> val_vec;
-      str_split(val_str, val_vec, sep, true);
+
+      if (split_header(key)) {
+        str_split(val_str, val_vec, sep, true);
+      } else {
+        str_trim(val_str);
+        val_vec.push_back(val_str);
+      }
+
       if (res.find(key) != res.end()) {
         res[key].insert(res[key].end(), val_vec.begin(), val_vec.end());
       }
