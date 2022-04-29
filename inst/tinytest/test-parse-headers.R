4bf813af89ce3652be9a9e708d93200e6ee9a2ef
David Zimmermann-Kollenda <david.zimmermann-kollenda@genre.com>
2022-04-29 09:34:38 +0200

    add test to cpp_parse_headers

--- inst/tinytest/test-parse-headers.R
+++ inst/tinytest/test-parse-headers.R
@@ -25,12 +25,15 @@ Connection: keep-alive\r\n
 Cookie: prov=f1e12498-1231-c8f0-8f53-97a8a6b17754; notice-ctt=4%3B1560153827826; mfnes=6e32CJ0CEAMaCwishdGGwpPLNxAFIJsCKAEyCDYyZGY0OTJh; acct=t=cmBi7gQEMWgxdi6kOiPwqAVNqmbEPdVj&s=E9Ly%2bCeEeAGmK9wDx2Zaseg6tiyi2hd8; sgt=id=3f4b96f5-b5ef-4ab1-96af-5ebce2950bcc\r\n
 Upgrade-Insecure-Requests: 1\r\n
 Cache-Control: max-age=0\r\n
-Custom_name: custom value
-TE: Trailers\r\n\r\n"
+Custom_name: custom value\r\n
+TE: Trailers\r\n
+If-Modified-Since: Tue, 15 Mar 2022 10:27:07 GMT\r\n
+If-Match: abc, def, ghi, jkl\r\n
+If-None-Match: abc, def, ghi, jkl\r\n"
 
 parsed = cpp_parse_headers(h)
 expect_true(inherits(parsed, "list"))
-expect_equal(length(parsed), 13L)
+expect_equal(length(parsed), length(strsplit(h, "\r\n")[[1]]))
 expect_equal(parsed[["connection"]], "keep-alive")
 expect_equal(parsed[["accept"]], c("text/html", "application/xhtml+xml", "application/xml"))
 expect_equal(parsed[["user-agent"]], "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0")
@@ -39,4 +42,49 @@ expect_equal(parsed[["accept-language"]], c("ru-RU", "ru"))
 expect_equal(length(parsed[["cookie"]]), 5L)
 expect_equal(parsed[["cookie"]][[1L]], "prov=f1e12498-1231-c8f0-8f53-97a8a6b17754")
 expect_equal(parsed[["custom_name"]], "custom value")
+expect_equal(parsed[["if-modified-since"]], "Tue, 15 Mar 2022 10:27:07 GMT")
+expect_equal(parsed[["if-match"]], c("abc", "def", "ghi", "jkl"))
+expect_equal(parsed[["if-none-match"]], c("abc", "def", "ghi", "jkl"))
+
+
+# automatically test that all headers which are splittable are split correctly
+split_headers = c(
+  "accept",
+  "accept-charset",
+  "access-control-request-headers",
+  "accept-encoding",
+  "accept-language",
+  "accept-patch",
+  "accept-ranges",
+  "allow",
+  "cache-control",
+  "connection",
+  "content-encoding",
+  "content-language",
+  "cookie",
+  "expect",
+  "forwarded",
+  "if-match",
+  "if-none-match",
+  "pragma",
+  "proxy-authenticate",
+  "te",
+  "trailer",
+  "transfer-encoding",
+  "upgrade",
+  "vary",
+  "via",
+  "warning",
+  "www-authenticate",
+  "x-forwarded-for"
+)
+
+for (h in split_headers) {
+    cont = "abc,def,ghi, jkl"
+    sep = if (h == "cookie") "; ?" else ", ?"
+    exp = strsplit(cont, sep)[[1]]
+    parsed = cpp_parse_headers(paste0(h, ": ", cont))
+    expect_equal(parsed[[h]], exp)
+}
+
 # nolint end
